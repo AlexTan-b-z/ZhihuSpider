34919ff0e32609dc28ea568dc01fa43ad759a0ce
AlexTan-b-z <572455105@qq.com>
2017-10-08 15:33:36 +0800

    Add status to avoid deleting useful ip
    Add status to avoid deleting useful ip

--- zhihu/zhihu/middlewares.py
+++ zhihu/zhihu/middlewares.py
@@ -10,7 +10,7 @@ import threading
 import pdb
 from scrapy import signals
 from .user_agents_pc import agents
-from .proxy import initIPPOOLS, removeIPPOOLS
+from .proxy import initIPPOOLS, updateIPPOOLS
 from .cookie import initCookie, updateCookie, removeCookie
 from scrapy.utils.response import response_status_message
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
@@ -49,40 +49,42 @@ class ProxyMiddleware(RetryMiddleware):
     def process_request(self,request,spider):
         #pdb.set_trace()
         ipNum=len(self.rconn.keys('IP*'))
-        if ipNum<20:
+        #pdb.set_trace()
+        if ipNum<50:
             proxy_thread = threading.Thread(target= initIPPOOLS,args = (self.rconn,))
             proxy_thread.setDaemon(True)
             proxy_thread.start()
             #initIPPOOLS(self.rconn)
-        if self.TIMES >= 3:
+        if self.TIMES == 3:
             baseIP=random.choice(self.rconn.keys('IP:*'))
             ip=str(baseIP,'utf-8').replace('IP:','')
             try:
-                IP=ip.split(':')[0]
-                PORT=ip.split(':')[1]
-                telnetlib.Telnet(IP,port=PORT,timeout=2) #测试ip是否可以用
+                IP,PORT,status=ip.split(':')
+                request.meta['status'] = status
+                telnetlib.Telnet(IP,port=PORT,timeout=2) #测试ip是否有效
             except:
-                removeIPPOOLS(self.rconn,ip)
+                logger.warning("The ip is not available !( IP:%s )" % ip)
+                updateIPPOOLS(self.rconn,IP+':'+PORT,status)
             else:
                 #pdb.set_trace()
-                self.IP = "http://" + ip
-                print("当前使用的代理IP是："+self.IP)
+                self.IP = "http://" + IP + ':' + PORT
+                logger.warning("The current IP is %s!" % self.IP)
                 self.TIMES = 0
+                updateIPPOOLS(self.rconn,IP+':'+PORT,status,1)
                 #pdb.set_trace()
         else:
             self.TIMES += 1
-        if self.IP is not '':
+        #pdb.set_trace()
+        if self.IP is not "":
             request.meta["proxy"] = self.IP
-            #pdb.set_trace()
 
     def process_response(self,request,response,spider):
-        #pdb.set_trace()
-        if response.status in [400,403,404,429,500,503,504]:
-            self.IP = 10
-            logger.error("%s! 错误..." % response.status)
+        if response.status in [400,403,404,429,500,502,503,504]:
+            self.TIMES = 3
+            logger.error("%s! error..." % response.status)
             #pdb.set_trace()
             try:
-                removeIPPOOLS(self.rconn,request.meta['proxy'].replace('http://',''))
+                updateIPPOOLS(self.rconn,request.meta['proxy'].replace('http://',''),request.meta['status'],-1)
             except:
                 pass
             reason = response_status_message(response.status)
@@ -92,14 +94,12 @@ class ProxyMiddleware(RetryMiddleware):
 
     def process_exception(self, request, exception, spider):
         #pdb.set_trace()
-        self.IP = 10
+        self.TIMES = 3
         try:
-            removeIPPOOLS(self.rconn,request.meta['proxy'].replace('http://',''))
+            updateIPPOOLS(self.rconn,request.meta['proxy'].replace('http://',''),request.meta['status'],-1)
         except:
             pass
         return request
-        #pdb.set_trace()
-
 
 class CookiesMiddleware(RetryMiddleware):
     """ 维护Cookie """
